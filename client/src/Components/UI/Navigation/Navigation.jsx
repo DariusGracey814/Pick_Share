e4bc6d6cafc598bb63dcff4dc4257e40bf607fa0
DariusGracey <Dariusgracey814@gmail.com>
2022-12-02 19:01:47 -0500

    create navigation component

---
+++ client/src/Components/UI/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <nav>
+      {/* Logo */}
+      <div></div>
+
+      {/* Mobile Menu */}
+
+      {/* Navigation List */}
+      <ul></ul>
+    </nav>
+  );
+}
+
+export default Navigation;
