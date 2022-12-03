2eb397059d88bb101f759c9d1a6dd3cd55cea1e4
DariusGracey <Dariusgracey814@gmail.com>
2022-12-02 19:03:43 -0500

    add react router setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
@@ -8,4 +9,8 @@ const container = document.getElementById("root");
 // Specify root
 const root = createRoot(container);
 // Render app in root container
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
