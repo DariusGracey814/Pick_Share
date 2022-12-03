6ce6bd75ac837e02dfb7825ae46d5b75a384ef83
DariusGracey <Dariusgracey814@gmail.com>
2022-12-02 19:16:12 -0500

    navigation component

--- client/src/Components/UI/Header/Header.jsx
+++ client/src/Components/UI/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Container from "../Container/Container";
+import Navigation from "../Navigation/Navigation";
 
 function Header() {
   return (
     <header>
-      <Container></Container>
+      <Container>
+        <Navigation />
+      </Container>
     </header>
   );
 }
--- client/src/Components/UI/Navigation/Navigation.jsx
+++ client/src/Components/UI/Navigation/Navigation.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import logo from "../../../assets/logoIcon.png";
+
 function Navigation() {
   return (
     <nav>
       {/* Logo */}
-      <div></div>
+      <div>
+        <img src={logo} alt="logo icon" width="25" height="25" />
+        <p>
+          <span>Pick</span>
+          <span>Share</span>
+        </p>
+      </div>
 
       {/* Mobile Menu */}
 
       {/* Navigation List */}
-      <ul></ul>
+      <ul>
+        <ul>
+          <li>
+            <Link to="/signup">Sign up</Link>
+          </li>
+        </ul>
+      </ul>
     </nav>
   );
 }
