16d818ad9386b487c1d2e26ee18cb72e329f6a5a
DariusGracey <Dariusgracey814@gmail.com>
2022-12-04 14:56:25 -0500

    insert demo button and usage text

--- client/src/Components/UI/Forms/LoginForm.jsx
+++ client/src/Components/UI/Forms/LoginForm.jsx
@@ -2,37 +2,56 @@ import React from "react";
 
 function LoginForm() {
   return (
-    <form className="login-form mt-12">
-      <div className="grid mb-3">
-        <label className="text-white mb-2" htmlFor="userEmail">
-          Email Address:
-        </label>
-        <input
-          className="p-3 loginInput"
-          type="email"
-          name="email"
-          id="email"
-          required
-        />
-      </div>
-      {/* Password */}
-      <div className="grid">
-        <label className="text-white mb-2" htmlFor="passwords">
-          Password:
-        </label>
-        <input
-          className="p-3 loginInput"
-          type="password"
-          name="password"
-          id="password"
-          required
-        />
-      </div>
+    <div>
+      <p className="accent-dark m-0 pt-4">
+        Click try demo to access demo account
+      </p>
+      <form className="login-form mt-12">
+        <div className="grid mb-3">
+          <label className="text-white mb-2" htmlFor="userEmail">
+            Email Address:
+          </label>
+          <input
+            className="p-3 loginInput"
+            type="email"
+            name="email"
+            id="email"
+            required
+          />
+        </div>
+        {/* Password */}
+        <div className="grid">
+          <label className="text-white mb-2" htmlFor="passwords">
+            Password:
+          </label>
+          <input
+            className="p-3 loginInput"
+            type="password"
+            name="password"
+            id="password"
+            required
+          />
+        </div>
 
-      <button className="btn-yellow btn-login mt-8" type="button">
-        Login
-      </button>
-    </form>
+        <div>
+          <button
+            className="btn-yellow btn-login2 mt-8"
+            type="button"
+            aria-labeled="login"
+          >
+            Login
+          </button>
+
+          <button
+            className="btn-yellow btn-login2 mt-8"
+            type="button"
+            aria-labeled="try demo"
+          >
+            Try Demo
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
