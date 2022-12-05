84f5de543362a696e086067702252c358756f7e4
DariusGracey <Dariusgracey814@gmail.com>
2022-12-05 14:22:16 -0500

    update feed component

--- client/src/Components/users/Feed/Feed.jsx
+++ client/src/Components/users/Feed/Feed.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Feed() {
   return (
-    <div className="">
+    <div className="light-clr w-half h-main rounded-sm">
       <h1>posts</h1>
     </div>
   );
--- client/src/Components/users/HomeFeed.jsx
+++ client/src/Components/users/HomeFeed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { loginStateActions } from "../../../store/loginState";
 import Container from "../UI/Container/Container";
+import Feed from "./Feed/Feed";
 
 function HomeFeed() {
   const dispatch = useDispatch();
@@ -14,7 +15,9 @@ function HomeFeed() {
   return (
     <section className="dark-mid w-full h-screen">
       <Container>
-        <h1>Home Feed</h1>
+        <div className="pt-10 border border-red-600">
+          <Feed />
+        </div>
       </Container>
     </section>
   );
