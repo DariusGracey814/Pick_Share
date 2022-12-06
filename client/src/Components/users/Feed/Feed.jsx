1d7509fa78d1609db5107d4a3888176f943729ba
DariusGracey <Dariusgracey814@gmail.com>
2022-12-06 12:04:10 -0500

    create user stats component

--- client/src/Components/users/Feed/Feed.jsx
+++ client/src/Components/users/Feed/Feed.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Feed() {
   return (
-    <div className="light2-clr posts h-secondary rounded-2xl box-shadow"></div>
+    <div className="light2-clr posts h-secondary rounded-2xl box-shadow">
+      <div className="border border-red-300"></div>
+      <div className="border border-red-300"></div>
+      <div className="border border-red-300"></div>
+    </div>
   );
 }
 
---
+++ client/src/Components/users/UserStats/UserStats.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserStats() {
+  return <div className="border border-blue-600">UserStats</div>;
+}
+
+export default UserStats;
