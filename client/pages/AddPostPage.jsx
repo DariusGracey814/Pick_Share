9f41df9cb31a44165acc70b2320bb2a00c73ffb2
DariusGracey <Dariusgracey814@gmail.com>
2022-12-07 17:46:19 -0500

    create add post route

---
+++ client/pages/AddPostPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddPost from "../src/Components/users/AddPostForm/AddPost";
+
+function AddPostPage() {
+  return (
+    <section>
+      <AddPost />
+    </section>
+  );
+}
+
+export default AddPostPage;
