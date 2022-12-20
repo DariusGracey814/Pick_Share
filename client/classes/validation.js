c0109e15defa2af98d34e7dd59a15a9dd6c3f399
DariusGracey <Dariusgracey814@gmail.com>
2022-12-19 22:19:51 -0500

    create validation class for form validation

--- client/classes/validation.js
+++ client/classes/validation.js
@@ -1,1 +1,18 @@
-class Validation {}
+class Validation {
+  // Take user fields for validation
+  validateInput(regex, value) {
+    switch (regex) {
+      case "password":
+        break;
+    }
+  }
+
+  // validate email
+  validateEmail(email) {
+    const regex = /^[a-z-\' ]{1,50}/;
+    const result = email.match(regex);
+  }
+
+  // validate email
+  validatePassword(password) {}
+}
